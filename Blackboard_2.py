d86d105f7ec735e444072535e2a1f740b547e7c7
josephh27 <josephjasonsbuhain@iskolarngbayan.pup.edu.ph>
2022-01-20 18:43:30 +0800

    changes

--- Blackboard_2.py
+++ Blackboard_2.py
@@ -1,47 +1,43 @@
-#import random
-#def dice_roll(sides):
-#    result = random.randint(1, sides)
-#    print(result)
+#import blackboard as bld
+#car_list = [
+#    "GTR R35", "Maserati", "Ferrari", "Mustang"
+#    ]
 
-#dice_roll(5)
-#import keyword
-#import random
-#import sys
-#print(keyword.iskeyword("try"))
-#print(random.randrange(8, 10, 2))
-#sys.stdout.write("print")
-#sys.stdout.write("\nokay")
-#valid = True
-#import sys
-#while valid:
-#    try:
-#        int(input("Input a number: "))
-#        valid = False
-#    except ValueError:
-#        print("Please input an integer.")
-    
-#def user_input(prompt):
-#    try:
-#        return input(prompt)
-#    except KeyboardInterrupt:
-#        print("Please press CTRL + C again to exit the program.")
-#user_input("Please enter something: ")
+#cars_prompt = [
+#    bld.car("Black", 350, "new"),
+#    bld.car("Red", 450, "old"),
+#    bld.car("Yellow", 379, "new")
+#    ]
 
-#def test(number):
-#    try:
-#        return int(input(number))
-#    except ValueError:
-#        print("Accepting integers only.")
-        
-#test("Input something for god's sake: ")
-import sys
-try:
-    1/0
-except ZeroDivisionError:
-    sys.exit("You divided by zero")
-while a1:
-    print("Okay", "Nah", sep="^")
-    a1 = False
-print("Okay bro")
+#def car_show(cars):
+#    for car in cars:
+#        print("Car color:", car.color, "<> Car horsepower:", car.horsepower)
+#        print(car.brand_new())
+  
+
+#print(cars_prompt[0].brand_new())
+
+#car_show(cars_prompt)
+
+from blackboard import masterChef, doggo
+class Chef(masterChef, doggo):
+    def make_sandwich(self):
+        print("*Sandwich made*")
+
+    def make_adobo(self):
+        print("*Adobo made*")
+
+    def make_hotdog(self):
+        print("*Hotdog made*")
 
     
+
+
+the_chef = Chef()
+the_chef.make_specialty()
+the_chef.eat_poop()
+
+random_list = ["dog", "cat", "alligator", "crocodile"]
+random_list1 = random_list.copy()
+random_list1.clear()
+print(random_list)
